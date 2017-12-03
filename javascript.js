1e89e3e098f7e95852784dd9a497faa2be21e2b7
Jeff Hartman <applejeff@mac.com>
2017-12-02 19:25:46 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
+{
+  "manifest_version": 2,
 
+  "name": "Research Buddy",
+  "description": "This extension creates a virtual assistant to diversify internet searches.",
+  "version": "1.0",
+
+  "browser_action": {
+    "default_icon": "icon.png",
+    "default_popup": "popup.html"
+  },
+  "permissions": [
+    "activeTab",
+    "storage"
+  ]
+}
